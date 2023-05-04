b8a07d5f1137e4de50c5815a129e9c0c46aeb344
Jenny Sun <jennysunnjm@gmail.com>
2023-05-04 16:46:02 -0400

    Analysis Part 2

--- googlesearch.py
+++ googlesearch.py
@@ -8,16 +8,30 @@ SEARCH_ENGINE_ID = os.environ.get("SEARCH_ENGINE_ID")
 API_KEY = "AIzaSyDTWD9wDMkNqcNVmDYhe2opu3gLC0ImG0s"
 SEARCH_ENGINE_ID = "a5627f229323d40e8"
 
+def inverse_cdf(u):
+    return int(1000 * (u**2))
+
+def sample_elements(arr, num_samples):
+    sampled_indices = set()
+    while len(sampled_indices) < num_samples:
+        u = np.random.uniform(0, 1)
+        idx = inverse_cdf(u)
+        if idx < len(arr):
+            sampled_indices.add(idx)
+    sampled_elements = [arr[i] for i in sorted(sampled_indices)]
+    return sampled_elements
+
 # Read search terms from file
-with open("analysis_llama_perp.txt", "r") as infile:
+with open("gpt-2-imdb_perp_prompt.txt", "r") as infile:
     file_content = infile.read()
-    SEARCH_TERMS = ast.literal_eval(file_content)[:100]
+    SEARCH_TERMS = sample_elements(ast.literal_eval(file_content), 100)
+    # SEARCH_TERMS = ast.literal_eval(file_content)[:100]
 
 total_search_terms = len(SEARCH_TERMS)
 successful_searches = 0
 
 # Open a text file named "results.txt" for saving search results
-with open("analysis_llama_cdf.txt", "w", encoding="utf-8") as results_file:
+with open("analysis_imdb_cdf.txt", "w", encoding="utf-8") as results_file:
     for query in SEARCH_TERMS:
         url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q=%22{query}%22"
         response = requests.get(url)
